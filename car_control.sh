1067be1d4285912b5a95839e3f7d07c0ae50c469
linskybing <linskybing@gamil.com>
2025-06-16 08:33:24 +0000

    update location unity

--- car_control.sh
+++ car_control.sh
@@ -1,6 +1,3 @@
 #! /bin/bash
 
-kubectl exec -it ros2-slam-unity -c pros-car -n $USER -- /bin/bash
-# kubectl exec -it ros2-discovery-server -n $USER -- /bin/bash
-# kubectl exec -it ros2-slam-unity -n $USER -- /bin/bash
-# fastdds discovery -i 0 -l 127.0.0.1 -p 11811
+kubectl exec -it ros2-localization-unity -c pros-car -n $USER -- /bin/bash
